81b27434b98e4f52eca6bf7fc94f41725a6e23ad
Sathish Jayapal <sjayapal@skminfotech.com>
2023-06-26 18:27:42 -0500

    Initial commit

---
+++ src/main/java/me/sathish/azurewebapp1/Application.java
@@ -0,0 +1,15 @@
+package me.sathish.azurewebapp1;
+
+import me.sathish.azurewebapp1.config.ApplicationProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({ApplicationProperties.class})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/me/sathish/azurewebapp1/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package me.sathish.azurewebapp1;
+
+import me.sathish.azurewebapp1.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {}
+}
